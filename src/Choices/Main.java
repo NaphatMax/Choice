bb377793c1dc06ce5fb4f1ee5e1baf17b0f77974
NaphatMax <76238426+NaphatMax@users.noreply.github.com>
2021-03-01 23:26:38 +0700

    -customize main menu with easteregg -exit function

--- src/Choices/Main.java
+++ src/Choices/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainMenuController.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
         primaryStage.setTitle("Choice");
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
--- src/Choices/MainMenuController.java
+++ src/Choices/MainMenuController.java
@@ -1,4 +1,64 @@
 package Choices;
 
-public class MainMenuController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class MainMenuController implements Initializable {
+
+    @FXML
+    Pane easterEgg;
+
+    @FXML
+    Text titleText;
+
+    @FXML
+    Text author;
+
+    @FXML
+    Button startButton;
+
+    @FXML
+    Button quitButton;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        titleText.setText("Choices Game");
+        author.setText("By AnAmazon");
+        startButton.setText("Start Game");
+        quitButton.setText("Quit");
+    }
+
+    public void EasterEggHover(){
+        easterEgg.setStyle("-fx-background-color: red");
+    }
+
+    public void EasterEggNoHover(){
+        easterEgg.setStyle("-fx-background-color: white");
+    }
+
+    public void EasterEggHoverRainbow(){
+        String[] colors ={"green", "blue", "red", "yellow", "purple", "pink"};
+
+        int rand = new Random().nextInt(colors.length);
+        String color = colors[rand];
+
+        easterEgg.setStyle("-fx-background-color: " +color);
+    }
+
+    public  void startButton(){
+
+    }
+
+    public void quitButton(){
+       System.exit(0);
+    }
+
 }
